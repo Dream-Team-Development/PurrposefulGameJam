c41382afd0d21b5dc31352ac5e50418f5d23075f
Lizzie <lizzie.briggs10@gmail.com>
2020-10-10 18:45:23 +0100

    Implemented hitting note at certain position and getting/losing points
    Not yet implemented specific keys to hit, only mapped to space bar for now

--- Assets/Scripts/RhythmGame/MusicNote.cs
+++ Assets/Scripts/RhythmGame/MusicNote.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RhythmGame
 {
@@ -7,14 +6,15 @@ namespace RhythmGame
     public class MusicNote : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _spriteRenderer;
-        
+
+        [SerializeField] private int _points = 10;
         [SerializeField] private Vector2 _spawnPos = new Vector2(0, 5);
-        [SerializeField] private Vector2 _removePos = new Vector2(0, -5);
+        [SerializeField] private Vector2 _hitPos = new Vector2(0, 5);
+        [SerializeField] private Vector2 _despawnPos = new Vector2(0, -5);
 
         private SongManager _songManager;
 
         public Vector2 SpawnPos => _spawnPos;
-        public Vector2 RemovePos => _removePos;
 
 
         private void Start()
@@ -27,15 +27,37 @@ namespace RhythmGame
 
         private void Update()
         {
+            // Stop moving notes if music is not playing
             if(!_songManager.Audio.isPlaying) return;
 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (transform.position.y <= _hitPos.y + 1 && transform.position.y >= _hitPos.y - 1)
+                {
+                    Debug.Log("Hit at " + transform.position.y);
+                    PointsManager.Instance.Points += _points;
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    Debug.Log("Missed at " + transform.position.y);
+                    PointsManager.Instance.Points -= _points / 2;
+                    Destroy(gameObject);
+                }
+            }
+
+            // Move note in time to song
             transform.position = Vector2.Lerp(
                 _spawnPos,
-                _removePos,
+                _despawnPos,
                 (_songManager.BeatsShownInAdvance - (_songManager.Notes[_songManager.NextIndex - 1] - _songManager.SongPosInBeats)) / _songManager.BeatsShownInAdvance
             );
-            
-            if(transform.position.y == _removePos.y) Destroy(this);
+
+            if (transform.position.y == _despawnPos.y)
+            {
+                PointsManager.Instance.Points -= _points;
+                Destroy(gameObject);
+            }
 
         }
     }
