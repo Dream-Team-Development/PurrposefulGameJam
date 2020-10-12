cb18d8fa6989e28935c8abc6f1664a6ca8e51337
Ian <barrowian1991@gmail.com>
2020-10-12 16:00:02 +0100

    Added spawner and colour changing rave party

--- Assets/Scripts/SushiGame/Controller/AiAgent.cs
+++ Assets/Scripts/SushiGame/Controller/AiAgent.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace SushiGame.Controller
 {
@@ -7,10 +8,20 @@ namespace SushiGame.Controller
         [SerializeField] private float _speed;
         [SerializeField] private AiController _aiController;
         
+        private Color[] _colors = {Color.blue, Color.cyan, Color.green, Color.magenta, Color.red, Color.yellow};
+        
         public int _startPos;
         public int _nextPos;
+
+        public AiController ControllerAi
+        {
+            get => _aiController;
+            set => _aiController = value;
+        }
+
         private void Awake()
         {
+            _aiController = FindObjectOfType<AiController>();
             _startPos = Random.Range(0, _aiController.Positions.Length);
             transform.position = _aiController.Positions[_startPos];
         }
@@ -26,6 +37,46 @@ namespace SushiGame.Controller
             if (!(Vector3.Distance(transform.position, _aiController.Positions[_nextPos]) < 0.2f)) return;
             if (_startPos == _aiController.Positions.Length - 1) _startPos = 0;
             else _startPos++;
+        }
+
+        public IEnumerator DeathTime()
+        {
+            var time = 10f;
+            var currentTime = 0f;
+
+            while (currentTime < time)
+            {
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+
+        public IEnumerator ColorChange()
+        {
+            var alwaysTrue = true;
+            var render = GetComponent<SpriteRenderer>();
+            var current = Random.Range(0, _colors.Length);
+            var next = current + 1;
+            var t = 0f;
+
+            if (current == _colors.Length - 1) next = 0;
+
+            while (alwaysTrue)
+            {
+                if (render.color == _colors[next])
+                {
+                    t = 0;
+                    current++;
+                    if (current == _colors.Length - 1) next = 0;
+                    else if (current == _colors.Length) current = 0;
+                    else next = current + 1;
+                }
+
+                t += Time.deltaTime;
+                render.color = Color.Lerp(_colors[current], _colors[next], t);
+                yield return null;
+            }
         }
     }
 }
