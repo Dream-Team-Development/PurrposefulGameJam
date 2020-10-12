e6b9c71fa0502feaea3e547c35db2fc701e60fe7
Lizzie <lizzie.briggs10@gmail.com>
2020-10-12 22:18:19 +0100

    Added basic cat scriptable objects
    Just takes in name, profile image, ideal weight

---
+++ Assets/Scripts/CatObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cat", menuName = "Cat")]
+
+[System.Serializable]
+public class CatObject : ScriptableObject
+{
+    public string catName;
+    public Sprite profileImage;
+
+    public int idealWeight;
+}
