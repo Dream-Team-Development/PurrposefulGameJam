93e7ca0c52718cbc0f682e7f125b46056fb116f2
Lizzie <lizzie.briggs10@gmail.com>
2020-10-12 11:53:05 +0100

    Added floating text when note hit, fixed notes piling when not hit
    Found bug with controls - controls are linked so need to separate

---
+++ Assets/Scripts/RhythmGame/FloatingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RhythmGame
+{
+    public class FloatingScript : MonoBehaviour
+    {
+        [SerializeField] private float _destroyTime = 1f;
+        [SerializeField] private Vector3 _randomisePos = new Vector3(1f, 1,0);
+
+        private void Start()
+        {
+            Destroy(gameObject, _destroyTime);
+        
+            transform.localPosition += new Vector3(
+                Random.Range(-_randomisePos.x, _randomisePos.x),
+                Random.Range(-_randomisePos.y, _randomisePos.y),
+                0);
+        }
+    }
+}
---
+++ Assets/Scripts/RhythmGame/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace RhythmGame
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private MusicNote _musicNote;
+        [SerializeField] private GameObject _hitPoint;
+
+        private void Start()
+        {
+            _musicNote.HitPos = _hitPoint;
+        }
+    }
+}
